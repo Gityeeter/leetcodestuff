d5b56ad7612d7a7656f750c6eb94abc2e47b53ea
Gityeeter <94848105+Gityeeter@users.noreply.github.com>
2023-05-10 19:48:21 -0700

    Time: 508 ms (26.52%), Space: 51.2 MB (5.83%) - LeetHub

--- 15-3sum/15-3sum.java
+++ 15-3sum/15-3sum.java
@@ -1,59 +1,41 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-    //    HashMap<Integer, Integer> numbers = new HashMap<Integer, Integer>();
+        Arrays.sort(nums);
         List<List<Integer>> answer = new ArrayList<List<Integer>>();
-        HashMap<List<Integer>, Integer> list_map = new HashMap<List<Integer>, Integer>();
-        for(int i =0; i< nums.length -2 ; i++)
+        Set<List<Integer>> hashy = new HashSet<>();
+        for(int i = 0;i< nums.length;i++)
         {
-            HashMap<Integer, Integer> numbers = new HashMap<Integer, Integer>();
-            int adjusted_total = -1 * nums[i];
-            for(int j =i+1; j< nums.length ; j++)
+            int left = i+1;
+            int right = nums.length -1;
+            int target = -nums[i];
+             System.out.println(target);
+            while(left < right)
             {
-                if(numbers.containsKey(nums[j]))
-                {
-                    numbers.put(nums[j],numbers.get(nums[j]) + 1);
-                }
-                else
-                {
-                    numbers.put(nums[j],1);
-                }
-                
-            }
-         //   System.out.println("fixing " + nums[i]);
-            for(int j =i+1; j< nums.length ; j++)
-            {
-           //     System.out.println("2nd is " + nums[j]);
-                int required = adjusted_total - nums[j];
-                if(numbers.containsKey(required))
-                {
-                    if(numbers.get(required) == 1)
-                    {
-                        numbers.remove(nums[j]);
-                    }
-                    else
-                    {
-                        numbers.put(nums[j],numbers.get(nums[j]) - 1);
-                    }
-                }
-                
-                if(numbers.containsKey(required))
+               int current = nums[left] + nums[right];
+               
+                if( current == target)
                 {
                     List<Integer> temp = new ArrayList<Integer>();
                     temp.add(nums[i]);
-                    temp.add(nums[j]);
-                    temp.add(required);
-                    Collections.sort(temp);
-                    if(!list_map.containsKey(temp))
+                    temp.add(nums[left]);
+                    temp.add(nums[right]);
+                    if(!hashy.contains(temp))
                     {
                         answer.add(temp);
-                        list_map.put(temp,1);
+                        hashy.add(temp);
                     }
-                    
-                    // System.out.println("adding.... " + temp);
+                   
+                    right--;
                 }
-             //   numbers.put(nums[j],nums[j]);
+                else if(current > target)
+                {
+                    right--;
+                }
+                else
+                {
+                    left++;
+                }
             }
-            
         }
         return answer;
     }
